ca0c3b42a96ce3b7c04303bb61f13a4b728c5de4
Joseph Lara <joseph.lara@gmail.com>
2025-06-16 15:23:25 +0200

    **dev** add functions for tbl building

--- Scripts/utilities.R
+++ Scripts/utilities.R
@@ -22,3 +22,52 @@ columns_to_remove <- c("calc_custo_instrumentos",
                        "calc_custo_deslocacao",
                        "calc_custo_total",
                        "calc_financiamento_lacuna")
+
+
+
+gen_tbl_activities <- function(df, breakdown = subactividade_tipo) {
+  
+  df %>%
+    group_by({{ breakdown }}) %>%
+    summarise(
+      n = n(),
+      across(
+        c(calc_custo_total, financiamento_oe, financiamento_prosaude, financiamento_outro_total, calc_financiamento_lacuna),
+        ~ sum(.x, na.rm = TRUE)
+      ),
+      .groups = "drop"
+    ) %>%
+    bind_rows(
+      df %>%
+        summarise(
+          {{ breakdown }} := "Total",
+          n = n(),
+          calc_custo_total = sum(calc_custo_total, na.rm = TRUE),
+          financiamento_oe = sum(financiamento_oe, na.rm = TRUE),
+          financiamento_prosaude = sum(financiamento_prosaude, na.rm = TRUE),
+          financiamento_outro_total = sum(financiamento_outro_total, na.rm = TRUE),
+          calc_financiamento_lacuna = sum(calc_financiamento_lacuna, na.rm = TRUE)
+        )
+    )
+}
+
+
+
+gen_tbl_financiador <- function(df, breakdown = financiador_detalhe) {
+  
+  df %>%
+    group_by({{ breakdown }}) %>%
+    summarise(
+      n = n(),
+      financiamento = sum(valor, na.rm = TRUE),
+      .groups = "drop"
+    ) %>%
+    bind_rows(
+      df %>%
+        summarise(
+          {{ breakdown }} := "Total",
+          n = n(),
+          financiamento = sum(valor, na.rm = TRUE)
+        )
+    )
+}
