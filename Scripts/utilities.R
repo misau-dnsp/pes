a581b6151c2ee7f3e8e151ab8a4ed7a435752eb9
Joseph Lara <joseph.lara@gmail.com>
2025-06-16 10:56:58 +0200

    **dev** transition to final kobo data structure

--- Scripts/utilities.R
+++ Scripts/utilities.R
@@ -1,7 +1,7 @@
 calc_budget_vars <- function(df) {
   df <- df %>% 
     rowwise() %>%
-    mutate(calc_custo_total = sum(c_across(starts_with("custo_")), na.rm = TRUE),
+    mutate(calc_custo_total = sum(c(c_across(starts_with("custo_")), calc_custo_contratacao), na.rm = TRUE),
            calc_financiamento_lacuna = calc_custo_total - calc_financiamento) %>% 
     ungroup()
 }
@@ -19,7 +19,6 @@ columns_to_label <- c("responsavel_programa",
 # variables to remove due to calculation NaN
 columns_to_remove <- c("calc_custo_instrumentos",
                        "calc_custo_estudo",
-                       "calc_custo_contratacao",
                        "calc_custo_deslocacao",
                        "calc_custo_total",
                        "calc_financiamento_lacuna")
