bdf59030f18bbdadf096a43bbcc9d254b84a088f
Joseph Lara <joseph.lara@gmail.com>
2025-06-19 17:39:28 +0200

    pesoe output development

---
+++ Scripts/pesoe.R
@@ -0,0 +1,176 @@
+
+# LOAD DEPENDENCIES ---------------------------------------------------------
+
+
+library(tidyverse)
+library(ggthemes)
+library(robotoolbox)
+library(glamr)
+library(dm)
+library(haven)
+library(gt)
+library(glue)
+
+
+# LOAD DEPENDENCIES ---------------------------------------------------------
+
+
+acct_kobo <- "kobo-jlara"
+acct_kobo_con <- get_account(name = acct_kobo)
+
+kobo_token(username = acct_kobo_con$username,
+           password = acct_kobo_con$password,
+           url = acct_kobo_con$url)
+
+main_vars <- c(
+  "_index",
+  "responsavel_programa",
+  "objectivo_pess", 
+  "actividade_principal_descricao",
+  "actividade_principal_indicador",
+  "actividade_principal_meta",
+  "subactividade_tipo",
+  "subactividade_tipo_outro_espec",
+  "subactividade_descricao",
+  "subactividade_beneficiario",
+  "subactividade_indicador",
+  "subactividade_meta",
+  "subactividade_local",
+  "subactividade_local_inter_deta",
+  "subactividade_local_reg_detalh",
+  "subactividade_local_prov_detal",
+  "financiamento_oe",
+  "financiamento_prosaude",
+  "financiamento_outro_total",
+  "calc_financiamento_lacuna"
+)
+
+columns_to_label <- c("responsavel_programa", 
+                      "responsavel_pf_ma", 
+                      "subactividade_tipo", 
+                      "objectivo_especifico", 
+                      "objectivo_pess", 
+                      "subactividade_local")
+
+
+tbl_objectivos_esp <- tibble::tibble(
+  actividade_principal = paste0("objectivo_especifico_", paste0("objective_esp_", 1:20)),
+  label = c(
+    "Reduzir a morbi-mortalidade através da expansão e melhoria da Qualidade dos Cuidados e Serviços de Saúde Materna",
+    "Expandir e melhorar a Qualidade dos Cuidados e Serviços de Saúde Sexual e Reprodutiva",
+    "Reduzir a mortalidade em crianças menores de 5 anos",
+    "Contribuir para a redução da morbimortalidade infantil, por desnutrição",
+    "Reduzir a prevalência e mortalidade das doenças preveníveis através de vacinas",
+    "Aumentar o número de adultos e de crianças vivendo com HIV que beneficiam de TARV",
+    "Reduzir a taxa de transmissão do HIV de mãe para filho",
+    "Contribuir para a redução da incidência e da morbimortalidade por tuberculose",
+    "Reduzir para metade a morbi-mortalidade por malária",
+    "Reduzir o peso das DNT e mitigar o seu impacto socioeconómico",
+    "Reduzir a prevalência das doenças tropicais negligenciadas na comunidade",
+    "Melhorar o acesso e a qualidade dos Cuidados e Serviços de Saúde Sexual e Reprodutiva para Adolescentes e Jovens",
+    "Contribuir para o estabelecimento de um ambiente escolar seguro, saudável e favorável à boa aprendizagem e ao desenvolvimento harmonioso do aluno",
+    "Contribuir para a redução do peso da doença através da adopção de estilos de vida saudáveis e da redução dos comportamentos de risco para a saúde",
+    "Prevenir e reduzir a morbilidade causada por perturbações mentais e de comportamento, doenças neuropsiquiátricas e distúrbios psicossociais, incluindo o consumo abusivo de drogas, sobretudo álcool e tabaco",
+    "Contribuir para a redução da ocorrência/frequência de surtos/epidemias com ênfase na erradicação da Pólio e eliminação do Sarampo",
+    "Contribuir para o fortalecimento da capacidade de gestão de emergências de saúde pública, deteção precoce e resposta atempada a eventos de saúde pública",
+    "Contribuir para a redução da incidência e prevalência de doenças relacionadas com determinantes ambientais de saúde",
+    "Contribuir para o bem-estar de saúde dos atletas e praticantes do exercício físico",
+    "Estabelecer um programa de saúde da terceira idade com vista a melhorar a qualidade de vida dos idosos"
+  )
+)
+
+
+# LOAD KOBO DATA ----------------------------------------------------------
+
+
+# timestamp for indicating data of data pull
+dt <- today()
+
+# fetch kobo assets using account credentials
+assets <- kobo_asset_list()
+
+uid <- assets %>%
+  filter(name == "DNSP PES 2026 Final") %>%
+  pull(uid) %>%
+  first()
+
+asset_list <- kobo_asset(uid)
+
+asset_df <- kobo_submissions(asset_list)
+
+rm(assets, asset_list, uid)
+
+
+# DRAW SCHEMA ------------------------------------------------------------
+
+
+dm_draw(asset_df)
+
+
+# CREATE MAIN DF --------------------------------------------------------------
+
+
+# Create main dataframe that will be built on
+df <- asset_df$main %>%
+  select(any_of(main_vars)) %>%
+  mutate(
+    across(any_of(columns_to_label), as_factor)
+  ) %>% glimpse()
+
+
+# CREATE OBJECTIVES DF ----------------------------------------------------
+
+
+# Create dataframe for activity and subactivity codes
+df_codigos <- asset_df$main %>% 
+  select(`_index`,
+         responsavel_programa,
+         starts_with("objectivo_especifico_objective")) %>% 
+  mutate(across(any_of(columns_to_label), as_factor)) %>% 
+  pivot_longer(cols = !c(`_index`,
+                         responsavel_programa),
+               names_to = "actividade_principal",
+               values_to = "value") %>% 
+  mutate(actividade_principal = str_replace_all(actividade_principal, "actividade_principal", "")) %>% 
+  filter(value == 1) %>% 
+  group_by(`_index`) %>%
+  slice_head(n = 1) %>%
+  ungroup() %>% 
+  left_join(tbl_objectivos_esp, by = "actividade_principal") %>% 
+  group_by(responsavel_programa) %>%
+  mutate(
+    atividade_id = dense_rank(actividade_principal),
+    codigo_actividade = paste0("DNSP-", responsavel_programa, "-A", atividade_id)
+  ) %>%
+  ungroup() %>% 
+  group_by(responsavel_programa, actividade_principal) %>%
+  mutate(codigo_subactividade = paste0(codigo_actividade, ".", row_number())) %>%
+  ungroup() %>% 
+  select('_index',
+         codigo_actividade,
+         codigo_subactividade)
+  
+  
+
+
+
+# ARCHIVE -----------------------------------------------------------------
+
+
+  pivot_longer(cols = c(responsavel_programa, 
+                        objectivo_pess,
+                        subactividade_tipo,
+                        subactividade_local),
+               names_to = "indicator",
+               values_to = "value")
+
+  
+    
+  # left_join(map_label,
+  #           by = c("value" = "indicator")
+  # ) |> 
+  select(!c(value,
+            actividade_principal)) |> 
+  pivot_wider(names_from = indicator,
+              values_from = label) %>%
+  mutate(subactividade_meta_label = as.character(subactividade_meta_label))
